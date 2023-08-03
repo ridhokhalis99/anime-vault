341d050db6007cba4bee49aebb49d542a4d66808
ridhokhalis99 <ridhokholis9a@gmail.com>
2023-08-03 22:35:52 +0700

    feat: adding meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,13 @@ const MyDocument = () => {
 
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Mantine nextjs example" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
